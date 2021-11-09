3c5e65f8d94e7347de80231f2da37523580da4af
Notkok <notkok@yandex.ru>
2021-11-09 18:52:48 +0300

    Реализован импорт/экспорт данных в формате эксель-файла

--- api/admin.py
+++ api/admin.py
@@ -19,29 +19,6 @@ class UserCustomAdmin(ImportExportActionModelAdmin):
     list_filter = ('userType', )
     filter_horizontal = ()
 
-#
-# class UserCustomAdmin(UserAdmin):
-#     ordering = ('login',)
-#     list_display = ('id', 'email', 'login',  'userType', 'date_joined')
-#     search_fields = ('email', 'login',)
-#     readonly_fields = ('date_joined', 'last_login',)
-#     filter_horizontal = ()
-#     fieldsets = ((None, {
-#         'fields': (
-#             'login', 'email', 'password', "userType", "is_admin",
-#             "last_login",
-#
-#         )
-#     }),)
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('email', 'login', "userType", "is_admin", 'password1', 'password2'),
-#         }),
-#     )
-#     list_filter = ('userType', )
-
-
 class RefreshTokensAdmin(admin.ModelAdmin):
     list_display = ('userId', 'refreshToken', 'created_at', 'expiresIn')
     readonly_fields = ('refreshToken', 'userId',)
@@ -65,27 +42,6 @@ class ProtocolAdmin(ImportExportActionModelAdmin):
                     'spoiled_bulletins', 'valid_bulletins')
     filter_horizontal = ()
 
-
-# class ProtocolAdmin(admin.ModelAdmin):
-#     list_display = ('number_of_voters', 'number_of_bulletins',
-#                     'spoiled_bulletins', 'valid_bulletins')
-#     fieldsets = ((None, {
-#         'fields': (
-#             'number_of_voters',
-#             'number_of_bulletins',
-#             'spoiled_bulletins',
-#             'valid_bulletins'
-#
-#         )
-#     }),)
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('number_of_voters', 'number_of_bulletins', "spoiled_bulletins", "valid_bulletins",),
-#         }),
-#     )
-#     filter_horizontal = ()
-
 class VotingAreaResource(resources.ModelResource):
     user = fields.Field(column_name='user', attribute='user', widget=ForeignKeyWidget(User, 'login'))
     protocol = fields.Field(column_name='protocol', attribute='protocol', widget=ForeignKeyWidget(Protocol, 'id'))
@@ -100,27 +56,6 @@ class VotingAreaAdmin(ImportExportActionModelAdmin):
     list_filter = ('is_opened', )
     filter_horizontal = ()
 
-# class VotingAreaAdmin(admin.ModelAdmin):
-#     list_display = ('district', 'num_voting_area',
-#                     'is_opened', 'max_people', 'count_voters', 'user', 'protocol')
-#     fieldsets = ((None, {
-#         'fields': (
-#             'district', 'num_voting_area',
-#             'is_opened', 'max_people', 'count_voters', 'user', 'protocol'
-#
-#         )
-#     }),)
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('district', 'num_voting_area',
-#                        'is_opened', 'max_people', 'count_voters', 'user', 'protocol'),
-#         }),
-#     )
-#     search_fields = ('district', 'num_voting_area')
-#     list_filter = ('is_opened', )
-#     filter_horizontal = ()
-
 class ConsigmentResource(resources.ModelResource):
     class Meta:
         model = Consigment
@@ -130,22 +65,6 @@ class ConsigmentAdmin(ImportExportActionModelAdmin):
     list_display = ('id', 'name',)
     filter_horizontal = ()
 
-# class ConsigmentAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name',)
-#     fieldsets = ((None, {
-#         'fields': (
-#             'name',
-#
-#         )
-#     }),)
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('name', ),
-#         }),
-#     )
-#     filter_horizontal = ()
-
 class CandidateResource(resources.ModelResource):
     consigment = fields.Field(column_name='consigment', attribute='consigment', widget=ForeignKeyWidget(Consigment, 'name'))
     class Meta:
@@ -153,28 +72,10 @@ class CandidateResource(resources.ModelResource):
 
 class CandidateAdmin(ImportExportActionModelAdmin):
     resource_class = CandidateResource
-    list_display = ('id', 'full_name', 'is_self_promoted', 'consigment')
+    list_display = ('id', 'photo', 'full_name', 'is_self_promoted', 'consigment')
     search_fields = ('full_name',)
     list_filter = ('consigment',  'is_self_promoted')
     filter_horizontal = ()
-
-# class CandidateAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'full_name', 'is_self_promoted', 'consigment')
-#     fieldsets = ((None, {
-#         'fields': (
-#             'full_name', 'is_self_promoted', 'consigment',
-#
-#         )
-#     }),)
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('full_name', 'is_self_promoted', 'consigment'),
-#         }),
-#     )
-#     search_fields = ('full_name',)
-#     list_filter = ('consigment',  'is_self_promoted')
-#     filter_horizontal = ()
 
 class ResultResource(resources.ModelResource):
     candidate = fields.Field(column_name='candidate', attribute='candidate', widget=ForeignKeyWidget(Candidate, 'full_name'))
@@ -187,24 +88,6 @@ class ResultAdmin(ImportExportActionModelAdmin):
     list_display = ('id', 'count_votes', 'candidate',)
     filter_horizontal = ()
 
-
-# class ResultAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'count_votes', 'candidate',)
-#     fieldsets = ((None, {
-#         'fields': (
-#             'count_votes', 'candidate',
-#
-#         )
-#     }),)
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('count_votes', 'candidate',),
-#         }),
-#     )
-#     filter_horizontal = ()
-
-
 class TimeTurnoutResource(resources.ModelResource):
     num_voting_area = fields.Field(column_name='num_voting_area', attribute='num_voting_area', widget=ForeignKeyWidget(VotingArea, 'num_voting_area'))
     class Meta:
@@ -216,27 +99,6 @@ class TimeTurnoutAdmin(ImportExportActionModelAdmin):
     list_display = ('id', 'voting_area', 'add_time', 'count_voters')
     exclude = ('add_time',)
     filter_horizontal = ()
-
-# class TimeTurnoutAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'voting_area', 'add_time', 'count_voters')
-#
-#     exclude = ('add_time',)
-#
-#     fieldsets = ((None, {
-#         'fields': (
-#             'voting_area', 'count_voters',
-#         )
-#     }),)
-#
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': (
-#             'voting_area', 'count_voters',
-#         )
-#     }),)
-#
-#     filter_horizontal = ()
 
 class AdminSite(admin.AdminSite):
     site_title = 'Выборы мэра Москвы'
